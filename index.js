2e2ff2f864b438c9eb8ba7f6861032c86f60eda5
Imran <u1704107@student.cuet.ac.bd>
2020-09-07 12:01:01 +0600

    connected mongodb atlas

--- index.js
+++ index.js
@@ -2,15 +2,34 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+
+// eslint-disable-next-line no-unused-vars
+const dotenv = require('dotenv').config();
+
 const app = express();
+
+// const server = require('http').createServer(app);
+
+// const io = require('socket.io')(server);
 
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/', (req, res, next) => {
+app.use('/', (req, res) => {
   res.send('Hi');
 });
 
-app.listen(port);
+// io.on('connection', () => { /* … */ });
+
+mongoose
+  .connect(process.env.mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(
+    () => {
+      app.listen(port);
+    },
+  ).catch((err) => {
+    throw err;
+  });
